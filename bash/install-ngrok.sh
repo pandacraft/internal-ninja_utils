e39155d28dec1dd3cafedac05b0bc9daed947fa9
Alex Lombry <alex.lombry@gmail.com>
2019-05-24 09:51:35 +0200

    Inspect to true on ngrok

--- bash/install-ngrok.sh
+++ bash/install-ngrok.sh
@@ -7,14 +7,14 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install ngrok
 
 cat <<EOF > $HOME/.ngrok2/ngrok.yml
 authtoken: ngrok_token
-log_level: info
-log_format: json
-log: /var/log/ngrok.log
+#log_level: info
+#log_format: json
+#log: /var/log/ngrok.log
 region: eu
 tunnels:
   pandacraft:
     addr: 443
     proto: http
     subdomain: pandacraft
-    inspect: false
+    inspect: true
 EOF
